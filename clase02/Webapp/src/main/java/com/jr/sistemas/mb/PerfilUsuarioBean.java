7eeae09c1a4f00b932c657d8dde3235c3fceb969
Javier <jreyes.sistemas@outlook.com>
2021-04-27 22:46:00 -0500

    ejempljo calendar

---
+++ clase02/Webapp/src/main/java/com/jr/sistemas/mb/PerfilUsuarioBean.java
@@ -0,0 +1,39 @@
+package com.jr.sistemas.mb;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable  
+{
+	private static final long serialVersionUID = 1L;
+	
+	private String nombre;
+	private Date fechaNacimiento;
+	
+	public void actualizar() {
+		FacesContext
+		.getCurrentInstance()
+		.addMessage(null, new FacesMessage("Perfil actualizado"));
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+}
